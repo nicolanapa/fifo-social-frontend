47685a24441f36b6fe5c729c27e942afb83f2893
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-28 11:41:56 +0100

    chore!: Upgraded fully React Router to v7

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
 import "./styles/style.css";
 import { routes } from "./routes";
 
