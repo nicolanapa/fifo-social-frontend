4cabf369e41f20fdae7c6ec1ca14cdd036bab684
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-28 15:06:32 +0100

    feat: Added /login form and handle functions for form values
    Added form State for login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import { getLoginStatus } from "../scripts/getLoginStatus.js";
 
 function Login() {
     const [loginStatus, setLoginStatus] = useState(false);
+    const [form, setForm] = useState({ username: "", password: "" });
 
     useEffect(() => {
         async function fetchLoginStatus() {
@@ -11,6 +12,41 @@ function Login() {
 
         fetchLoginStatus();
     }, []);
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        const response = await fetch(
+            import.meta.env.VITE_SERVER_FULL_DOMAIN + "/login",
+            {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                method: "POST",
+                body: new URLSearchParams({
+                    username: form.username,
+                    password: form.password,
+                }),
+                credentials: "include",
+            }
+        );
+
+        const responseStatus = await response.json();
+
+        console.log(responseStatus);
+
+        if (responseStatus.isAuthenticated) {
+            setLoginStatus(responseStatus.isAuthenticated);
+        }
+    }
+
+    function handleUsername(e) {
+        setForm({ ...form, username: e.target.value });
+    }
+
+    function handlePassword(e) {
+        setForm({ ...form, password: e.target.value });
+    }
 
     return (
         <>
@@ -31,7 +67,33 @@ function Login() {
                 )}
                 <h2>Login</h2>
 
-                <form action="/login" method="POST"></form>
+                <form
+                    action={import.meta.env.VITE_SERVER_FULL_DOMAIN + "/login"}
+                    method="POST"
+                    onSubmit={handleLogin}
+                >
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={form.username}
+                        onChange={handleUsername}
+                        minLength={2}
+                        maxLength={64}
+                        required
+                    />
+
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={form.password}
+                        onChange={handlePassword}
+                        required
+                    />
+
+                    <button type="submit">Enter</button>
+                </form>
             </div>
             <div>
                 <h2>Signup</h2>
