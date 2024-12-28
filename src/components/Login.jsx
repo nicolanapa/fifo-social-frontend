f59a6b64be645a51c2b14837445cf110dd11802c
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-28 18:41:24 +0100

    feat: Signup Form and showing new user info along with the password

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import { getLoginStatus } from "../scripts/getLoginStatus.js";
 function Login() {
     const [loginStatus, setLoginStatus] = useState(false);
     const [form, setForm] = useState({ username: "", password: "" });
+    const [newUser, setNewUser] = useState({ username: "", password: "" });
 
     useEffect(() => {
         async function fetchLoginStatus() {
@@ -38,6 +39,34 @@ function Login() {
         if (responseStatus.isAuthenticated) {
             setLoginStatus(responseStatus.isAuthenticated);
         }
+    }
+
+    async function handleSignup(e) {
+        e.preventDefault();
+
+        const response = await fetch(
+            import.meta.env.VITE_SERVER_FULL_DOMAIN + "/user",
+            {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                method: "POST",
+                body: new URLSearchParams({
+                    username: e.target.username.value,
+                    description: e.target.description.value,
+                }),
+                credentials: "include",
+            }
+        );
+
+        const responseStatus = await response.json();
+
+        console.log(responseStatus);
+
+        setNewUser({
+            username: e.target.username.value,
+            password: responseStatus.password,
+        });
     }
 
     function handleUsername(e) {
@@ -100,7 +129,43 @@ function Login() {
             <div>
                 <h2>Signup</h2>
 
-                <form action="/user" method="POST"></form>
+                <form
+                    action={import.meta.env.VITE_SERVER_FULL_DOMAIN + "/user"}
+                    method="POST"
+                    onSubmit={handleSignup}
+                >
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={form.username}
+                        onChange={handleUsername}
+                        minLength={2}
+                        maxLength={64}
+                        required
+                    />
+
+                    <label htmlFor="description">Description</label>
+                    <input
+                        type="text"
+                        id="description"
+                        name="description"
+                        maxLength={150}
+                    />
+
+                    <button type="submit">Register</button>
+                </form>
+
+                {newUser.username !== "" && newUser.password !== "" ? (
+                    <div>
+                        <h2>Your new account was successfully created!</h2>
+                        <p>Username: {newUser.username}</p>
+                        <p>Password: {newUser.password}</p>
+                        <p>You can now log in!</p>
+                    </div>
+                ) : (
+                    ""
+                )}
             </div>
         </>
     );
