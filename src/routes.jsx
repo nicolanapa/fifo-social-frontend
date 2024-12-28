19052372d2dcaae6b047709543279ecb1444b658
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-28 14:06:19 +0100

    feat: Added / and /login path

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { Outlet } from "react-router";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Login from "./components/Login";
 
-const routes = [{}];
+const routes = [
+    {
+        path: "/",
+        element: (
+            <>
+                <NavBar />
+                <main>
+                    <Outlet />
+                </main>
+            </>
+        ),
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "login",
+                element: <Login />,
+            },
+        ],
+    },
+];
 
 export { routes };
