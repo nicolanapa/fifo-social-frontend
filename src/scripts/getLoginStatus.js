3dd5916ac362fba4949e88ae06dbd28ed239d062
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-28 14:07:22 +0100

    feat: Add script to get isAuthenticated value

---
+++ src/scripts/getLoginStatus.js
@@ -0,0 +1,19 @@
+async function getLoginStatus() {
+    const response = await fetch(
+        import.meta.env.VITE_SERVER_DOMAIN +
+            ":" +
+            import.meta.env.VITE_SERVER_PORT +
+            "/login",
+        {
+            method: "GET",
+        }
+    );
+
+    const loginObjectJson = await response.json();
+
+    console.log(loginObjectJson);
+
+    return loginObjectJson.isAuthenticated;
+}
+
+export { getLoginStatus };
