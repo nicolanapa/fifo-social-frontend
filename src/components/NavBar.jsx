db857a465cd7025e9a0ed8f0ccb01bf579a18864
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-29 22:32:48 +0100

    feat!: NavLinks and Icons and usage of LoginContext to keep track of user path

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,42 @@
+import { useContext, useEffect, useState } from "react";
+import { NavLink } from "react-router";
+import LoginContext from "../context/LoginContext.jsx";
+
 function NavBar() {
+    const [loginOrUserPath, setLoginOrUserPath] = useState("/login");
+    const loginInfo = useContext(LoginContext);
+
+    useEffect(() => {
+        async function fetchLoginStatus() {
+            if (loginInfo.isAuthenticated) {
+                setLoginOrUserPath("/user/" + loginInfo.id);
+            }
+        }
+
+        fetchLoginStatus();
+    }, []);
+
     return (
         <header>
             <nav>
-                <p>X</p>
+                <NavLink to="/">
+                    <img src="./icons/homeIcon.svg" alt="Home" />
+                </NavLink>
+                <div>
+                    <input type="text" />
+                    <button type="submit">
+                        <img src="./icons/searchIcon.svg" alt="Search" />
+                    </button>
+                </div>
+                <NavLink to="/addPost">
+                    <img src="./icons/addPostIcon.svg" alt="Add a Post" />
+                </NavLink>
+                <NavLink to={loginOrUserPath}>
+                    <img
+                        src="./icons/userIcon.svg"
+                        alt="Your account or login page"
+                    />
+                </NavLink>
             </nav>
         </header>
     );
