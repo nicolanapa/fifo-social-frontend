5a5fd3a1fcebd9418c95c7146d07ce8987317a69
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-30 22:05:35 +0100

    feat: Added routing of /errorPage and errorElement

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,14 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import App from "./components/App";
 import AddPost from "./components/AddPost";
+import ErrorPage from "./components/ErrorPage";
+import Page404 from "./components/Page404";
 
 const routes = [
     {
         path: "/",
         element: <App />,
+        errorElement: <Page404 />,
         children: [
             {
                 path: "/",
@@ -19,6 +22,10 @@ const routes = [
             {
                 path: "addPost",
                 element: <AddPost />,
+            },
+            {
+                path: "errorPage",
+                element: <ErrorPage />,
             },
         ],
     },
