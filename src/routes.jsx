bd5cf5823421f0f9ef2dbbc5e36f23fb7c909ad5
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-30 21:24:05 +0100

    feat: Added /addPost children

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import Home from "./components/Home";
 import Login from "./components/Login";
 import App from "./components/App";
+import AddPost from "./components/AddPost";
 
 const routes = [
     {
@@ -14,6 +15,10 @@ const routes = [
             {
                 path: "login",
                 element: <Login />,
+            },
+            {
+                path: "addPost",
+                element: <AddPost />,
             },
         ],
     },
