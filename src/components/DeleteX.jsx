0536c8ce92650f3f11d55bbb3f790694953e98d0
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-01-03 19:44:14 +0100

    feat!: Sending the DELETE request when an User is the owner of typeOfX
    Added dynamic navigation and reloading based on typeOfX

--- src/components/DeleteX.jsx
+++ src/components/DeleteX.jsx
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
 import { useContext, useEffect, useState } from "react";
 import LoginContext from "../context/LoginContext";
+import { getLoginObject } from "../scripts/getLoginObject";
+import { useNavigate } from "react-router";
 
 function DeleteX({ xId, typeOfX }) {
     const { loginInfo, setLoginInfo } = useContext(LoginContext);
     const [isOwner, setIsOwner] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         async function fetchX() {
@@ -27,6 +30,7 @@ function DeleteX({ xId, typeOfX }) {
             if (typeOfX === "user") {
                 setIsOwner(loginInfo.id === responseObject.id ? true : false);
             } else {
+                console.log(responseObject[0]);
                 setIsOwner(
                     loginInfo.id === responseObject[0].user_id ? true : false
                 );
@@ -36,10 +40,55 @@ function DeleteX({ xId, typeOfX }) {
         fetchX();
     }, []);
 
+    async function deleteThing() {
+        const updatedLoginInfo = await getLoginObject();
+
+        setLoginInfo(updatedLoginInfo);
+
+        if (updatedLoginInfo.isAuthenticated) {
+            const deleteResponse = await fetch(
+                import.meta.env.VITE_SERVER_DOMAIN +
+                    ":" +
+                    import.meta.env.VITE_SERVER_PORT +
+                    "/" +
+                    typeOfX +
+                    "/" +
+                    xId,
+                {
+                    method: "DELETE",
+                    credentials: "include",
+                }
+            );
+
+            const responseStatus = await deleteResponse.json();
+
+            console.log(responseStatus);
+
+            if (responseStatus.success) {
+                typeOfX === "user" || typeOfX === "post"
+                    ? navigate("/")
+                    : location.reload();
+            } else {
+                navigate("/errorPage", {
+                    state: {
+                        code:
+                            responseStatus.status !== ""
+                                ? responseStatus.status
+                                : "",
+                        msg:
+                            responseStatus.msg !== ""
+                                ? responseStatus.msg
+                                : "Couldn't delete the " + typeOfX,
+                    },
+                });
+            }
+        }
+    }
+
     return (
         <>
             {isOwner && (
-                <button>
+                <button onClick={deleteThing}>
                     <img
                         src="/icons/deleteIconFeather.svg"
                         alt={"Delete this " + typeOfX}
