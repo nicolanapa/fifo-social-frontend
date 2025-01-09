8f924fd5bb8319675d295642fd4a69ddfe4ef977
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-01-09 17:54:02 +0100

    feat: Added showing of Posts and Comments throug PostPreview and Comment
    Moved the keys generation at the top using instead States to fix a re-rendering bug that would reset the whole component (making More opening and closing due to the re-render after half a second)

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,10 +1,16 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import LoginContext from "../context/LoginContext";
 import { getLoginObject } from "../scripts/getLoginObject";
 import { Link } from "react-router";
+import PostPreview from "./PostPreview";
+import Comment from "./Comment";
 
 function Favorites() {
     const { loginInfo, setLoginInfo } = useContext(LoginContext);
+    const [posts, setPosts] = useState([]);
+    const [comments, setComments] = useState([]);
+    const [randomKeyPosts, setRandomKeyPosts] = useState([]);
+    const [randomKeyComments, setRandomKeyComments] = useState([]);
 
     async function fetchFavorites(id) {
         const responsePost = await fetch(
@@ -36,7 +42,22 @@ function Favorites() {
         const responsePostObject = await responsePost.json();
         const responseCommentObject = await responseComment.json();
 
-        console.log(responsePostObject, responseCommentObject);
+        const tempRandomKeysPosts = [];
+        const tempRandomKeysComments = [];
+
+        for (let i = 0; i < responsePostObject.length; i++) {
+            tempRandomKeysPosts.push(crypto.randomUUID());
+        }
+
+        for (let i = 0; i < responseCommentObject.length; i++) {
+            tempRandomKeysComments.push(crypto.randomUUID());
+        }
+
+        setRandomKeyPosts(tempRandomKeysPosts);
+        setRandomKeyComments(tempRandomKeysComments);
+
+        setPosts(await responsePostObject);
+        setComments(await responseCommentObject);
     }
 
     useEffect(() => {
@@ -55,7 +76,52 @@ function Favorites() {
     return (
         <>
             {loginInfo.isAuthenticated ? (
-                <></>
+                <>
+                    <section>
+                        <h2>Posts</h2>
+
+                        {posts.length !== 0 ? (
+                            posts.map((post, i) => {
+                                console.log("index", i, randomKeyPosts[i]);
+
+                                return (
+                                    <PostPreview
+                                        id={post.id}
+                                        user_id={post.user_id}
+                                        title={post.title}
+                                        content={post.content}
+                                        likes={post.likes}
+                                        creation_date={post.creation_date}
+                                        key={randomKeyPosts[i]}
+                                    />
+                                );
+                            })
+                        ) : (
+                            <h2>No Favorited posts yet...</h2>
+                        )}
+                    </section>
+                    <section>
+                        <h2>Comments</h2>
+
+                        {comments.length !== 0 ? (
+                            comments.map((comment, i) => {
+                                console.log("index", i, randomKeyComments[i]);
+                                return (
+                                    <Comment
+                                        commentId={comment.id}
+                                        user_id={comment.user_id}
+                                        content={comment.content}
+                                        likes={comment.likes}
+                                        creation_date={comment.creation_date}
+                                        key={randomKeyComments[i]}
+                                    />
+                                );
+                            })
+                        ) : (
+                            <h2>No Favorited comments yet...</h2>
+                        )}
+                    </section>
+                </>
             ) : (
                 <div>
                     <h1>You&apos;re not logged in</h1>
