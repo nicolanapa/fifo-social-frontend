87b58ce759ad439a6fc74c2f6be5ac507eee7236
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-01-17 23:02:47 +0100

    feat: Conditional rendering depending on state and default parameters
    Managed to find a solution when state is null and avoid an Error

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,3 +1,33 @@
-function SearchResult() {}
+import { useLocation } from "react-router";
+
+function SearchResult() {
+    const location = useLocation();
+    const state = location.state || {};
+
+    const { array = [] } = state;
+    let { typeOfSearch = "" } = state;
+
+    if (array.length === 0 && typeOfSearch !== "") {
+        typeOfSearch = "empty";
+    }
+
+    console.log(array.length);
+
+    return (
+        <>
+            {typeOfSearch === "empty" ? (
+                <p>No results found!</p>
+            ) : typeOfSearch === "global" ? (
+                <p>Global</p>
+            ) : typeOfSearch === "users" ? (
+                <p>User</p>
+            ) : typeOfSearch === "posts" ? (
+                <p>Post</p>
+            ) : (
+                <p>Search something first!</p>
+            )}
+        </>
+    );
+}
 
 export default SearchResult;
