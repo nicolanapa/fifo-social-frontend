6a9e54780f97de0cd96cba8f471fc4ea38821e9c
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-01-18 23:18:41 +0100

    feat: First part with html layout set up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,30 @@
+import { useContext } from "react";
+import LoginContext from "../context/LoginContext";
+
 function Home() {
+    const { loginInfo, setLoginInfo } = useContext(LoginContext);
+
     return (
         <>
-            <>Y</>
+            {loginInfo.isAuthenticated && (
+                <div>
+                    <h2>Hi, {loginInfo.username}</h2>
+                    <p>
+                        Here&apos;s the latest posts from your followed Users
+                        List!
+                    </p>
+                </div>
+            )}
+
+            <section>
+                <h2>Recommended People</h2>
+
+                {/* random users, UserPreview*/}
+            </section>
+
+            <section>
+                {/* random posts if not logged in / posts from followed user list if logged in*/}
+            </section>
         </>
     );
 }
