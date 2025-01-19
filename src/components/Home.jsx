145cc1ce05ae21d0fa2e73dfdf63b809bab09f80
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-01-19 11:45:13 +0100

    feat: Creating of a temporal posts Array to be set at the end
    For now for the authenticated size

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,8 @@ function Home() {
     const { loginInfo, setLoginInfo } = useContext(LoginContext);
     const [users, setUsers] = useState([]);
     const [posts, setPosts] = useState([]);
+    const [usersKey, setUsersKey] = useState([]);
+    const [postsKey, setPostsKey] = useState([]);
 
     // Reduce to one single fetch function with parameters
     // If they're not used for anything other than fetching
@@ -68,11 +70,36 @@ function Home() {
             const allUsers = await fetchUsers();
             let usersTemp = [];
             let postsTemp = [];
+            let usersKeyTemp = [];
+            let postsKeyTemp = [];
 
             if (loginInfo.isAuthenticated) {
                 const followedUsers = await fetchFollowed(loginInfo.id);
                 console.log(followedUsers);
                 console.log(await fetchFollowed(1));
+
+                for (let i = 0; i < followedUsers.length; i++) {
+                    if (i === 20) {
+                        break;
+                    }
+
+                    const allUserPosts = await fetchPosts(
+                        followedUsers[i].followed_id
+                    );
+
+                    if (allUserPosts.length === 0) continue;
+
+                    for (let i2 = 0; i2 < allUserPosts.length; i2++) {
+                        if (i2 === 10) {
+                            break;
+                        }
+
+                        postsTemp.push(allUserPosts[i2]);
+                        postsKeyTemp.push(crypto.randomUUID());
+                    }
+                }
+
+                console.log(postsTemp);
             }
         }
 
