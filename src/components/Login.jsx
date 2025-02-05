5c4eafe016b10c4c74e689a4a86c40c4b52cc3d9
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-02-05 19:43:04 +0100

    feat: Moved the isAuthenticated paragraph outside and added classes for forms and containers
    Changed divs with form to be a semantic tag instead, section

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -59,34 +59,36 @@ function Login() {
 
     return (
         <>
-            <div>
-                {loginInfo.isAuthenticated ? (
-                    <>
-                        <p>
-                            You&apos;re already logged in as{" "}
-                            {loginInfo.username} but you can still log in as
-                            another account or create a new one!
-                        </p>
-                    </>
-                ) : (
-                    <>
-                        <p>
-                            You aren&apos;t logged in. You should Login or
-                            Signup!
-                        </p>
-                    </>
-                )}
+            {loginInfo.isAuthenticated ? (
+                <>
+                    <p className="title-section bold">
+                        You&apos;re already logged in as {loginInfo.username}{" "}
+                        but you can still log in as another account or create a
+                        new one!
+                    </p>
+                </>
+            ) : (
+                <>
+                    <p className="title-section bold">
+                        You aren&apos;t logged in. You should Login or Signup!
+                    </p>
+                </>
+            )}
+
+            <section className="login-container">
                 <h2>Login</h2>
 
                 <form
                     action={import.meta.env.VITE_SERVER_FULL_DOMAIN + "/login"}
                     method="POST"
                     onSubmit={handleLogin}
+                    className="login-signup-form"
                 >
                     <label htmlFor="username">Username</label>
                     <input
                         type="text"
                         id="username"
+                        className="styled-input"
                         name="username"
                         minLength={2}
                         maxLength={64}
@@ -97,25 +99,30 @@ function Login() {
                     <input
                         type="password"
                         id="password"
+                        className="styled-input"
                         name="password"
                         required
                     />
 
-                    <button type="submit">Enter</button>
+                    <button type="submit" className="styled-button">
+                        Enter
+                    </button>
                 </form>
-            </div>
-            <div>
+            </section>
+            <section className="signup-container">
                 <h2>Signup</h2>
 
                 <form
                     action={import.meta.env.VITE_SERVER_FULL_DOMAIN + "/user"}
                     method="POST"
                     onSubmit={handleSignup}
+                    className="login-signup-form"
                 >
                     <label htmlFor="username">Username</label>
                     <input
                         type="text"
                         id="username"
+                        className="styled-input"
                         minLength={2}
                         maxLength={64}
                         required
@@ -125,24 +132,33 @@ function Login() {
                     <input
                         type="text"
                         id="description"
+                        className="styled-input"
                         name="description"
                         maxLength={150}
                     />
 
-                    <button type="submit">Register</button>
+                    <button type="submit" className="styled-button">
+                        Register
+                    </button>
                 </form>
 
                 {newUser.username !== "" && newUser.password !== "" ? (
-                    <div>
+                    <div className="title-section">
                         <h2>Your new account was successfully created!</h2>
-                        <p>Username: {newUser.username}</p>
-                        <p>Password: {newUser.password}</p>
+                        <p>
+                            Username:{" "}
+                            <span className="bold">{newUser.username}</span>
+                        </p>
+                        <p>
+                            Password:{" "}
+                            <span className="bold">{newUser.password}</span>
+                        </p>
                         <p>You can now log in!</p>
                     </div>
                 ) : (
                     ""
                 )}
-            </div>
+            </section>
         </>
     );
 }
