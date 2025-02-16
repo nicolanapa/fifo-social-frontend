2ad9b7f5bf3c220387360b367564f252e7f1dee6
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-02-16 15:30:58 +0100

    feat(style): Modal-like view when device's width is less than 500px
    Added State to track the modal

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -6,6 +6,7 @@ import "../styles/navBar.css";
 function SearchForm({ width }) {
     const [selectedOption, setSelectedOption] = useState("global");
     const [hiddenSelect, setHiddenSelect] = useState(true);
+    const [isOpenSmallModal, setIsOpenSmallModal] = useState(false);
     const navigate = useNavigate();
 
     async function handleSearching(e) {
@@ -33,66 +34,171 @@ function SearchForm({ width }) {
     }
 
     return (
-        <form
-            action={import.meta.env.VITE_SERVER_FULL_DOMAIN + "/search"}
-            method="GET"
-            onSubmit={handleSearching}
-            className="search-form"
-        >
-            <input
-                type="search"
-                name="searchInput"
-                id="search-input"
-                required
-            />
-
-            <button
-                type="button"
-                onClick={() => {
-                    setHiddenSelect(!hiddenSelect);
-                }}
-                className="styled-button"
-            >
-                <img
-                    className={(width < 800 ? "small" : "bigger") + "-image"}
-                    src="/icons/searchTypeIcon.svg"
-                    alt="Search options"
-                />
-            </button>
-
-            <div className={"relative-modal" + (hiddenSelect ? " hidden" : "")}>
-                <div className="absolute-search-modal">
-                    <label htmlFor="type-of-search" hidden={hiddenSelect}>
-                        Search by
-                    </label>
-                    <select
-                        name="typeOfSearch"
-                        id="type-of-search"
-                        value={selectedOption}
-                        onChange={(e) => setSelectedOption(e.target.value)}
-                        hidden={hiddenSelect}
+        <>
+            {width <= 500 ? (
+                <>
+                    <button
+                        type="submit"
+                        onClick={() => {
+                            setIsOpenSmallModal(!isOpenSmallModal);
+                        }}
+                        className="styled-button"
                     >
-                        <option value="global">All</option>
-                        <option value="users">Users</option>
-                        <option value="posts">Posts</option>
-                    </select>
-                </div>
-            </div>
-            {/*selectedOption === "global" ? (
-                <img src="/icons/userIcon.svg" alt="Global Search" />
-            ) : selectedOption === "users" ? (
-                <img src="/icons/userIcon.svg" alt="Search only Users" />
+                        <img
+                            className={
+                                (width < 800 ? "small" : "bigger") + "-image"
+                            }
+                            src="/icons/searchIcon.svg"
+                            alt="Search"
+                        />
+                    </button>
+
+                    {isOpenSmallModal && (
+                        <form
+                            action={
+                                import.meta.env.VITE_SERVER_FULL_DOMAIN +
+                                "/search"
+                            }
+                            method="GET"
+                            onSubmit={handleSearching}
+                            className="search-form absolute-search-form"
+                        >
+                            <input
+                                type="search"
+                                name="searchInput"
+                                id="search-input"
+                                required
+                            />
+
+                            <button
+                                type="button"
+                                onClick={() => {
+                                    setHiddenSelect(!hiddenSelect);
+                                }}
+                                className="styled-button"
+                            >
+                                <img
+                                    className={
+                                        (width < 800 ? "small" : "bigger") +
+                                        "-image"
+                                    }
+                                    src="/icons/searchTypeIcon.svg"
+                                    alt="Search options"
+                                />
+                            </button>
+
+                            <div
+                                className={
+                                    "relative-modal" +
+                                    (hiddenSelect ? " hidden" : "")
+                                }
+                            >
+                                <div className="absolute-search-modal">
+                                    <label
+                                        htmlFor="type-of-search"
+                                        hidden={hiddenSelect}
+                                    >
+                                        Search by
+                                    </label>
+                                    <select
+                                        name="typeOfSearch"
+                                        id="type-of-search"
+                                        value={selectedOption}
+                                        onChange={(e) =>
+                                            setSelectedOption(e.target.value)
+                                        }
+                                        hidden={hiddenSelect}
+                                    >
+                                        <option value="global">All</option>
+                                        <option value="users">Users</option>
+                                        <option value="posts">Posts</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <button type="submit" className="styled-button">
+                                <img
+                                    className={
+                                        (width <= 800 ? "small" : "bigger") +
+                                        "-image"
+                                    }
+                                    src="/icons/searchIcon.svg"
+                                    alt="Search"
+                                />
+                            </button>
+                        </form>
+                    )}
+                </>
             ) : (
-                "Post"
-            )*/}
-            <button type="submit" className="styled-button">
-                <img
-                    className={(width < 800 ? "small" : "bigger") + "-image"}
-                    src="/icons/searchIcon.svg"
-                    alt="Search"
-                />
-            </button>
-        </form>
+                <form
+                    action={import.meta.env.VITE_SERVER_FULL_DOMAIN + "/search"}
+                    method="GET"
+                    onSubmit={handleSearching}
+                    className="search-form"
+                >
+                    <input
+                        type="search"
+                        name="searchInput"
+                        id="search-input"
+                        required
+                    />
+
+                    <button
+                        type="button"
+                        onClick={() => {
+                            setHiddenSelect(!hiddenSelect);
+                        }}
+                        className="styled-button"
+                    >
+                        <img
+                            className={
+                                (width <= 800 ? "small" : "bigger") + "-image"
+                            }
+                            src="/icons/searchTypeIcon.svg"
+                            alt="Search options"
+                        />
+                    </button>
+
+                    <div
+                        className={
+                            "relative-modal" + (hiddenSelect ? " hidden" : "")
+                        }
+                    >
+                        <div className="absolute-search-modal">
+                            <label
+                                htmlFor="type-of-search"
+                                hidden={hiddenSelect}
+                            >
+                                Search by
+                            </label>
+                            <select
+                                name="typeOfSearch"
+                                id="type-of-search"
+                                value={selectedOption}
+                                onChange={(e) =>
+                                    setSelectedOption(e.target.value)
+                                }
+                                hidden={hiddenSelect}
+                            >
+                                <option value="global">All</option>
+                                <option value="users">Users</option>
+                                <option value="posts">Posts</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <button type="submit" className="styled-button">
+                        <img
+                            className={
+                                (width <= 800 ? "small" : "bigger") + "-image"
+                            }
+                            src="/icons/searchIcon.svg"
+                            alt="Search"
+                        />
+                    </button>
+                </form>
+            )}
+        </>
     );
 }
 
